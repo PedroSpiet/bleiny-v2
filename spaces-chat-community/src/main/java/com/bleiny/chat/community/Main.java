febf531ffca7b647ddd39d7bdee2b6968b0c67f3
PedroSpiet <tyroxbr@gmail.com>
2023-04-29 11:28:31 -0300

    spaces architecture expected

---
+++ spaces-chat-community/src/main/java/com/bleiny/chat/community/Main.java
@@ -0,0 +1,7 @@
+package com.bleiny.chat.community;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ spaces-community/spaces-community-domain/spaces-community-domain-service/src/main/java/com/bleiny/community/domain/service/Main.java
@@ -0,0 +1,7 @@
+package com.bleiny.community.domain.service;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
